04d3d97acd1edcdbe9fc6ff55e9496924fc93a76
ShreyasVihan <shreyasvihanshahjain@gmail.com>
2026-07-13 21:24:10 +0530

    Add existing Files

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -3,36 +3,32 @@
 #include <stdexcept>
 #include"Calculator.h"
 
-
-class Calculator {
-public:
-    double add(double a, double b) {
+    double Calculator::add(double a, double b) {
         return a + b;
     }
 
-    double subtract(double a, double b) {
+    double Calculator::subtract(double a, double b) {
         return a - b;
     }
 
-    double multiply(double a, double b) {
+    double Calculator::multiply(double a, double b) {
         return a * b;
     }
 
-    double divide(double a, double b) {
+    double Calculator::divide(double a, double b) {
         if (b == 0) {
             throw std::invalid_argument("Division by zero is not allowed.");
         }
         return a / b;
     }
 
-    double power(double base, double exponent) {
+    double Calculator::power(double base, double exponent) {
         return std::pow(base, exponent);
     }
 
-    double squareRoot(double value) {
+    double Calculator::squareRoot(double value) {
         if (value < 0) {
             throw std::invalid_argument("Square root of negative number is not allowed.");
         }
         return std::sqrt(value);
     }
-};
